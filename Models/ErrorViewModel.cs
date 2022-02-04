b30fffa5888a9be25b9573a72fc5b77563c0fc5f
Christian Harjuno <harjuno.tian@gmail.com>
2022-02-04 21:50:41 +0700

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ITCinemaFrontend.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
