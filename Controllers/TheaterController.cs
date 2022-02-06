5150e4a9861f3eeca480aa75b6b269acf9779a31
Abraham Aditya <abraham.sudjatmoko@binus.ac.id>
2022-02-06 13:28:56 +0700

    feat: add theater page

---
+++ Controllers/TheaterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITCinemaFrontend.Controllers
+{
+    public class TheaterController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (Request.QueryString.HasValue)
+            {
+                var query = System.Web.HttpUtility.ParseQueryString(Request.QueryString.Value);
+                if(query.Get("id") == null)
+                {
+                    return View();
+                }
+                return View("TheaterDetail");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
